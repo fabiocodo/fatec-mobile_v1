31a7d923937ca4dbebf7baa68daaa0cb9f5288e3
fabiocodo <91349289+fabiocodo@users.noreply.github.com>
2022-11-17 19:37:56 -0300

    first commit

---
+++ src/screens/signin/index.tsx
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles  } from './styles';
+export  function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Teste Helolo word</Text>
+    </View>
+  );
+}
---
+++ src/screens/signin/styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet} from 'react-native';
+
+ export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+        height:60,
+        width:180,
+        borderBottomWidth:2
+    },
+  });
